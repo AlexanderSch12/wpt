e39667f2ec2c941fc09849ccbc3d600152b098bd
AlexanderSch12 <alexander.schoeters@gmail.com>
2024-02-20 08:51:48 +0000

    Babel browser runtime implementation

--- resources/testharnessreport.js
+++ resources/testharnessreport.js
@@ -14,6 +14,110 @@
  * parameters they are called with see testharness.js
  */
 
+var errors = [];
+
+window.onerror = function(error, url, line) {
+    errors.push({error:error, url:url, line:line});
+};
+
+function init_babel()
+{
+    var scripts;
+    var scripts_content = [];
+    var babel_script;
+    
+    // babel_script = document.createElement("script");
+    // babel_script.src = "/resources/babel.min.js";
+    // babel_script.className = "babel_ignore";
+    // document.head.appendChild(babel_script);
+
+    
+        Babel.registerPreset("env-plus", {
+        presets: [[Babel.availablePresets["env"], {
+            "targets":
+            {
+              "chrome": 5
+            },
+            // "modules": "systemjs",
+            "useBuiltIns": "entry"
+          }]]
+        //   ,
+        // plugins: [
+        //   [
+        //     Babel.availablePlugins["plugin-transform-runtime"],
+        //           {
+        //   "regenerator": true
+        //     }
+        //   ],
+        // ],
+      });
+
+    // document.addEventListener("DOMContentLoaded", function() 
+    // {
+    scripts = document.getElementsByTagName("script");
+    for(var i = 0; i < scripts.length ; i++)
+    {
+        var script = scripts[i];
+        var splits = script.src.split("/");
+        if(splits[splits.length - 1] === "babel.min.js" || splits[splits.length - 1] === "testharnessreport.js" || splits[splits.length - 1] === "testharness_babel.js" )
+        {
+            script.className = "babel_ignore";
+        }
+        if(script.className !== "babel_ignore")
+        {
+            // if(!script.hasAttribute("src"))
+            // {
+            //     console.log("removed?");
+            //     scripts_content.push(script.innerHTML);
+            //     // script.remove();
+            // } else {
+                console.log("SET ATTRIBUTE");
+                script.setAttribute("type","text/babel");
+                script.setAttribute("data-presets", "env-plus");
+            // }
+        }
+    }
+    // });
+
+    document.addEventListener("DOMContentLoaded", function() 
+    {
+        console.log("DOM_LOADED");
+    });
+
+    // babel_script.addEventListener("load", function() {
+
+    //   console.log("BABEL LOADED");
+        // Babel.disableScriptTags();
+        // Babel.transformScriptTags();
+        // for(var i = 0; i < scripts_content.length ; i++)
+        // {
+        //     var script_content = scripts_content[i];
+        //     var output = Babel.transform(script_content,{ presets: ['env'] });
+        //     var new_script = document.createElement("script");
+        //     new_script.type = "text/javascript";
+        //     new_script.innerHTML = output.code;
+        //     document.body.appendChild(new_script);
+        // }
+    // });
+
+
+            // for(var i = 0; i < scripts.length ; i++)
+        // {
+        //     var script = scripts[i];
+        //     var splits = script.src.split("/");
+        //     if(splits[splits.length - 1] == "testharnessreport.js")
+        //     {
+        //         script.className = "babel_ignore";
+        //     }
+        //     if(script.className != "babel_ignore")
+        //     {
+        //         var output = Babel.transform(script.value,{ presets: ['env'] });
+        //         console.log(output.code);
+        //     }
+        // }
+
+}
+
 var xhr;
 var response_data;
 
@@ -31,13 +135,18 @@ function init_message()
     xhr = new XMLHttpRequest();
 }
 
+var results;
+
 function get_test_results(tests, status) 
 {
     // If 1 test fails, the requested test fails
     var result = true;
+    results = [];
     for(var i = 0 ; i < tests.length ; i++)
     {
         test_result = tests[i].format_status();
+        results.push(test_result);
+        console.log(test_result)
         if(test_result != 'Pass')
         {
             result = false;
@@ -54,18 +163,21 @@ function send_test_results(result)
     const endpoint = base_url + "/reporting/resources/bughog.py";
 
     response_data.wpt_result = result;
+    response_data.results = results;
+    response_data.errors = errors;
     var response_json = JSON.stringify(response_data);
     xhr.open("POST", endpoint, true);
     xhr.send(response_json);
 }
 
+init_babel();
 init_message();
 
 try{
     add_completion_callback(get_test_results);
 } catch(e)
 {
-    send_test_results(e.message);
+    send_test_results(errors);
 }
 
 
